276f34b5ce902837ccad349cdd5aa83f82f61dc5
Prabhat Lodhi <prabhat@navgurukul.org>
2026-02-04 22:38:56 +0530

    allocation page updated

--- features/allocations/index.js
+++ features/allocations/index.js
@@ -1,3 +1,5 @@
 export { default as AllocationsList } from './AllocationsList';
 export { default as AllocationDetails } from './AllocationDetails';
+export { default as CreateAllocation } from './CreateAllocation';
+
 
