8f9dcec9f2dae09c9370709198eb05d41118046a
Prabhat Lodhi <prabhat@navgurukul.org>
2026-02-05 13:11:45 +0530

    ticket dual scroll fix

--- app/tickets/page.js
+++ app/tickets/page.js
@@ -2,7 +2,7 @@ import TicketsList from '@/features/tickets/TicketsList';
 
 export default function TicketsPage() {
   return (
-    <div className="p-6 overflow-y-auto max-h-screen">
+    <div className="p-6 overflow-y-auto h-full">
       <TicketsList />
     </div>
   );
