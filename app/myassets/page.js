8b50718f691734ab3af44fddc6f5d47703ba4664
Prabhatlodhi <lodhi.prabhat@gmail.com>
2026-04-04 02:42:55 +0530

    padding fix

--- app/myassets/page.js
+++ app/myassets/page.js
@@ -1,7 +1,7 @@
 import { MyAssetsTab } from "@/features/userprofile/tabs";
 
 export default function MyAssetsPage() {
-  return <div>
+  return <div className="p-8">
     <MyAssetsTab />
   </div>;
 }
