ec5b0ed9f2977ab0b5484e9c45cfe3bfcf344d72
Prabhatlodhi <lodhi.prabhat@gmail.com>
2026-04-04 03:08:17 +0530

    Add dark mode support and include features directory in Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 const { nextui } = require("@nextui-org/react");
 
 module.exports = {
+  darkMode: "class",
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./features/**/*.{js,ts,jsx,tsx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
