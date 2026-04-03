5efc5fac7426d7f02deab6d4f8d45b316600ae1c
Prabhatlodhi <lodhi.prabhat@gmail.com>
2026-04-04 03:02:55 +0530

    component added

--- app/myassets/page.js
+++ app/myassets/page.js
@@ -1,7 +1,8 @@
-import { MyAssetsTab } from "@/features/userprofile/tabs";
+import { MyAssetsTab, UserProfileTab } from "@/features/userprofile/tabs";
 
 export default function MyAssetsPage() {
   return <div className="p-8">
+  <UserProfileTab />
     <MyAssetsTab />
   </div>;
 }
