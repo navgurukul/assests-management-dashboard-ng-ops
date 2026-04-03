0fe4a95295ee438f0ceb8b047cfa2a1b943e3f52
Prabhatlodhi <lodhi.prabhat@gmail.com>
2026-04-03 12:24:17 +0530

    contenxt added

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import "./globals.css";
 import ReactQueryProvider from "./helper/ReactQueryProvider";
 import ReduxProvider from "./helper/ReduxProvider";
 import { AuthProvider } from "@/app/context/AuthContext";
+import { ThemeProvider } from "@/app/context/ThemeContext";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { Toaster } from "sonner";
 
@@ -24,23 +25,25 @@ export const metadata = {
 
 function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${poppins.variable} antialiased`}>
         <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}>
           <ReduxProvider>
             <ReactQueryProvider>
               <AuthProvider>
-                <ClientProviders>
-                  <ClientLayout>
-                    {children}
-                  </ClientLayout>
-                  <Toaster 
-                    position="top-right" 
-                    richColors 
-                    expand={false}
-                    closeButton
-                  />
-                </ClientProviders>
+                <ThemeProvider>
+                  <ClientProviders>
+                    <ClientLayout>
+                      {children}
+                    </ClientLayout>
+                    <Toaster 
+                      position="top-right" 
+                      richColors 
+                      expand={false}
+                      closeButton
+                    />
+                  </ClientProviders>
+                </ThemeProvider>
               </AuthProvider>
             </ReactQueryProvider>
           </ReduxProvider>
