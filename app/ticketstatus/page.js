cd2128886997d4c36dde15d6e95ae90d6ee961fa
Prabhatlodhi <lodhi.prabhat@gmail.com>
2026-04-08 15:26:44 +0530

    Refactor TicketStatusPage: replace TicketsTable with TicketStatusTab for improved user interface

--- app/ticketstatus/page.js
+++ app/ticketstatus/page.js
@@ -1,7 +1,8 @@
 import TicketsTable from "@/features/tickets/TicketsTable";
+import { TicketStatusTab } from "@/features/userprofile/tabs";
 
 export default function TicketStatusPage() {
   return <div className="" >
-    <TicketsTable/>
+    <TicketStatusTab/>
   </div>;
 }
