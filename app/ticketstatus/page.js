093390aa3201d565cc39fc34fa3fc70a20e2b4fe
Prabhatlodhi <lodhi.prabhat@gmail.com>
2026-05-09 08:30:28 +0530

    scroll fix in both the tab

--- app/ticketstatus/page.js
+++ app/ticketstatus/page.js
@@ -2,7 +2,9 @@ import TicketsTable from "@/features/tickets/TicketsTable";
 import { TicketStatusTab } from "@/features/userprofile/tabs";
 
 export default function TicketStatusPage() {
-  return <div className="" >
-    <TicketStatusTab/>
-  </div>;
+  return (
+    <div className="h-full flex flex-col overflow-hidden">
+      <TicketStatusTab />
+    </div>
+  );
 }
